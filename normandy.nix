5dc416e35acbfc1072c58e1fa7176b1781ee9ec8
Rhys Davies <rhys@memes.nz>
2021-10-17 15:56:58 +1300

    Use a list

--- normandy.nix
+++ normandy.nix
@@ -130,8 +130,8 @@
   
   nix = {
     autoOptimiseStore = true;
-    binaryCaches = "s3://nix-cache?region=us-east-1&endpoint=nix-cache.memes.nz";
-    binaryCachePublicKeys = "nix-cache:JW9dxrc5qdmyDkUstVqjlVcBpunX4Jo6ueshaJIWCK4=";
+    binaryCaches = [ "s3://nix-cache?region=us-east-1&endpoint=nix-cache.memes.nz" ];
+    binaryCachePublicKeys = [ "nix-cache:JW9dxrc5qdmyDkUstVqjlVcBpunX4Jo6ueshaJIWCK4=" ];
     package = pkgs.nixUnstable;
     extraOptions = ''
       experimental-features = nix-command flakes
