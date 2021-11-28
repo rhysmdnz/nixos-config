5d4a81061ab8502b5443ab727263b00ca7277958
Rhys Davies <rhys@memes.nz>
2021-11-28 21:30:47 +1300

    Jamie can have a little access as a treat

--- normandy.nix
+++ normandy.nix
@@ -138,6 +138,14 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICwWm3Yv/f8pmUfZIm8SvsbrewsNcpUHpJ3zrODSt/0 rhys@tempest" ];
   };
+  users.users.jamie = {
+    uid = 1001;
+    isNormalUser = true;
+    home = "/home/jamie";
+    description = "Jamie";
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmjgKWGrFYlHDY67GEaOhH32DgxbucL/XNlSROXQjWU hydra@hydra" ];
+  };
 
   nix.buildMachines = [
     {
