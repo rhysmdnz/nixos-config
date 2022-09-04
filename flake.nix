28db07b1ed3b80c91a35b7a6b118de04f1fe04b1
Rhys Davies <rhys@memes.nz>
2022-09-04 21:27:03 +1200

    auto-formatting and significant refactoring at the same time

--- flake.nix
+++ flake.nix
@@ -10,61 +10,75 @@
     emacs.url = "github:nix-community/emacs-overlay";
   };
 
-  outputs = { self, nixpkgs, nixpkgsTweaks, flake-compat-ci, home-manager, nix-doom-emacs, emacs, ... }: {
+  outputs = { self, nixpkgs, nixpkgsTweaks, flake-compat-ci, home-manager, nix-doom-emacs, emacs, ... }:
 
-    nixosConfigurations.normandy = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        { nixpkgs.overlays = [ emacs.overlay ]; }
-        ./normandy.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.rhys = {
-            imports = [ nix-doom-emacs.hmModule ./home.nix ];
-          };
-        }
-      ];
+    let
+      patchedNixpkgs = nixpkgs.legacyPackages.x86_64-linux.applyPatches {
+        name = "patched-nixpkgs-source";
+        src = nixpkgs.outPath;
+        patches = [
+          (nixpkgs.legacyPackages.x86_64-linux.fetchpatch {
+            url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/189676.patch";
+            sha256 = "sha256-wOw+Lie8x8/rtXvxAsNoCWZrRB6PndHBCXO/ZYDbYgQ=";
+          })
+        ];
+      };
+      coolNixosSystem = import "${patchedNixpkgs}/nixos/lib/eval-config.nix";
+    in
+    {
+      nixosConfigurations.normandy = coolNixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          { nixpkgs.overlays = [ emacs.overlay ]; }
+          ./normandy.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.rhys = {
+              imports = [ nix-doom-emacs.hmModule ./home.nix ];
+            };
+          }
+        ];
+      };
+
+      #nixosConfigurations.normandyTest = nixpkgsHardened.lib.nixosSystem {
+      #  system = "x86_64-linux";
+      #  modules = [
+      #    { nixpkgs.overlays = [ emacs.overlay ]; }
+      #    ./normandy.nix
+      #    home-manager.nixosModules.home-manager
+      #    {
+      #      home-manager.useGlobalPkgs = true;
+      #      home-manager.useUserPackages = true;
+      #      home-manager.users.rhys = {
+      #        imports = [ nix-doom-emacs.hmModule ./home.nix ];
+      #      };
+      #    }
+      #  ];
+      #};
+
+      nixosConfigurations.elbrus = nixpkgsTweaks.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          { nixpkgs.overlays = [ emacs.overlay ]; }
+          ./elbrus.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.rhys = {
+              imports = [ nix-doom-emacs.hmModule ./home.nix ];
+            };
+          }
+        ];
+      };
+
+      hydraJobs.build.normandy = self.nixosConfigurations.normandy.config.system.build.toplevel;
+      hydraJobs.build.normandyTest = self.nixosConfigurations.normandyTest.config.system.build.toplevel;
+      ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
+        flake = self;
+        systems = [ "x86_64-linux" ];
+      };
     };
-
-    #nixosConfigurations.normandyTest = nixpkgsHardened.lib.nixosSystem {
-    #  system = "x86_64-linux";
-    #  modules = [
-    #    { nixpkgs.overlays = [ emacs.overlay ]; }
-    #    ./normandy.nix
-    #    home-manager.nixosModules.home-manager
-    #    {
-    #      home-manager.useGlobalPkgs = true;
-    #      home-manager.useUserPackages = true;
-    #      home-manager.users.rhys = {
-    #        imports = [ nix-doom-emacs.hmModule ./home.nix ];
-    #      };
-    #    }
-    #  ];
-    #};
-
-    nixosConfigurations.elbrus = nixpkgsTweaks.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        { nixpkgs.overlays = [ emacs.overlay ]; }
-        ./elbrus.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.rhys = {
-            imports = [ nix-doom-emacs.hmModule ./home.nix ];
-          };
-        }
-      ];
-    };
-
-    hydraJobs.build.normandy = self.nixosConfigurations.normandy.config.system.build.toplevel;
-    hydraJobs.build.normandyTest = self.nixosConfigurations.normandyTest.config.system.build.toplevel;
-    ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
-      flake = self;
-      systems = [ "x86_64-linux" ];
-    };
-  };
 }
