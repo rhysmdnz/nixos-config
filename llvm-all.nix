670599f5c2018e23890a56152460bd9ef0efaa35
Rhys Davies <rhys@memes.nz>
2023-01-11 01:34:12 +1300

    test systemd thing

--- llvm-all.nix
+++ llvm-all.nix
@@ -89,6 +89,7 @@
         );
         gst_all_1 = super.gst_all_1 // { gst-plugins-bad = super.gst_all_1.gst-plugins-bad.override { stdenv = badstdenv; }; };
         libapparmor = super.libapparmor.override { stdenv = badstdenv; };
+        systemd = super.systemd.override { stdenv = badstdenv; };
       })
     ];
 
