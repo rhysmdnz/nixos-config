43547ad4e6bc751b160020b3ad33a1b8b250ac30
Rhys Davies <rhys@memes.nz>
2023-09-19 15:30:14 +1200

    Remove pkgconfig

--- elbrus.nix
+++ elbrus.nix
@@ -23,12 +23,10 @@
     clang
     vscode
     gnumake
-    pkgconfig
     openssl
     virtiofsd
     #qemu
   ];
-  environment.sessionVariables.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
   virtualisation.kvmgt.enable = true;
   virtualisation.kvmgt.vgpus = {
