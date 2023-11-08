844f9a00105f1d5ca693032653c1d31494cbf3e2
Rhys Davies <rhys@memes.nz>
2023-11-08 13:11:47 +1300

    Don't auto optimise store on mac

--- nix-conf.nix
+++ nix-conf.nix
@@ -4,7 +4,7 @@
   nix = {
     gc.automatic = true;
     gc.options = "-d";
-    settings.auto-optimise-store = true;
+    settings.auto-optimise-store = if pkgs.stdenv.isDarwin then false else true;
     settings.trusted-users = [ "rhys" ];
     settings.substituters = [ "https://nix-cache.memes.nz/nix-cache/" ];
     settings.trusted-public-keys = [ "nix-cache:JW9dxrc5qdmyDkUstVqjlVcBpunX4Jo6ueshaJIWCK4=" ];
