ca2471739f6d0a2551ae345e5c939c14b86fe70a
Rhys Davies <rhys@memes.nz>
2024-02-08 09:46:25 +1300

    use new cache

--- nix-conf.nix
+++ nix-conf.nix
@@ -6,8 +6,8 @@
     gc.options = "-d";
     settings.auto-optimise-store = if pkgs.stdenv.isDarwin then false else true;
     settings.trusted-users = [ "rhys" ];
-    settings.substituters = [ "https://nix-cache.memes.nz/nix-cache/" ];
-    settings.trusted-public-keys = [ "nix-cache:JW9dxrc5qdmyDkUstVqjlVcBpunX4Jo6ueshaJIWCK4=" ];
+    settings.substituters = [ "https://nix-cache.memes.nz/nix-cache/" "https://cache.memes.nz/cache" ];
+    settings.trusted-public-keys = [ "nix-cache:JW9dxrc5qdmyDkUstVqjlVcBpunX4Jo6ueshaJIWCK4=" "cache:/89NJtgM/IWySqvXSsfNiWWOhSdXcOj6AmHZcVkwLyA=" ];
     package = pkgs.nixUnstable;
     generateNixPathFromInputs = true;
     linkInputs = true;
